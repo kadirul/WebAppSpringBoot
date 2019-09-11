07c6c85575e97dfd9fc1293d56bdc007d205e9f5
Kadirul <kadirul@ymail.com>
2019-09-11 12:41:45 -0400

    finished my first webapp using spring boot

--- src/main/java/com/nobinwebapp/app/MyController.java
+++ src/main/java/com/nobinwebapp/app/MyController.java
@@ -6,17 +6,18 @@ import javax.servlet.http.HttpSessionAttributeListener;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
 	@RequestMapping("home")
-	public String home(/* HttpServletRequest req */String name, HttpSession session) {
+	public ModelAndView home(Alien al) {
 		
-		//HttpSession session = req.getSession();
-		//String name = req.getParameter("name");
-		System.out.println("Hi " + name);
-		session.setAttribute("name", name);
-		return "home";
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("obj", al);
+		mv.setViewName("home");
+		return mv;
 	}
 }
